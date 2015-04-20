ee3ae232509792a63a10528931012ef700c45879
mcostasoftware <mcostasoftware@gmail.com>
2015-04-20 20:02:54 +0100

    Working Tags
    tags are now working

--- client/views/templates/homepage/homepage.js
+++ client/views/templates/homepage/homepage.js
@@ -3,5 +3,9 @@ Meteor.subscribe('theSuggestions');
 Template.homepage.helpers({
     suggestion : function () {
         return Suggestions.find();
+    },
+    tags : function(){
+        var tagArray = _.pluck(Suggestions.find().fetch(), 'tags' );
+        return tagArray;
     }
 });
